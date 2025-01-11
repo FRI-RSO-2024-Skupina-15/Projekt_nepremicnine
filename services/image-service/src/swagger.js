85a36c587f313b7ab72b1c6ccf8a96b513dfe95e
kristofzupan <kristofzupan00@gmail.com>
2025-01-11 14:34:09 +0100

    fixed swagger

--- services/image-service/src/swagger.js
+++ services/image-service/src/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://72.146.60.142/api/images',
+        url: 'http://72.146.60.142',
         description: 'Production server',
       },
       {
